67be1be0818000c902aeae09482319cfc1b728c4
MurilloConti <murilloconti@hotmail.com>
2019-10-14 12:49:45 -0300

    databases

---
+++ selects.sql
@@ -0,0 +1,18 @@
+﻿SELECT * from rendimentos;
+-- evoluçao de patrimonio
+  select CONCAT(MONTH(DataFim),'/',YEAR(DataFim)) as mmYYYY,sum(SaldoFinal)  as total FROM rendimentos GROUP BY(DataFim)
+-- posição atual
+  select rendimentos.SaldoFinal, fundos.Nome from rendimentos
+    inner join fundos on fundos.Id = rendimentos.FundoId
+    where rendimentos.DataFim = 20190930
+    GROUP BY(FundoId);
+
+  -- valorização dos fundos 
+  select rendimentos.PercLucro, fundos.Nome from rendimentos
+    inner join fundos on fundos.Id = rendimentos.FundoId
+    where rendimentos.DataFim = 20190930
+    GROUP BY(FundoId);
+
+    
+-- percentual de aumento de patrimonio
+  select CONCAT(MONTH(DataFim),'/',YEAR(DataFim)) as mmYYYY, IFNULL(TRUNCATE(((sum(SaldoFinal) - sum(SaldoAnterior))*100)/sum(SaldoAnterior),2),0) as percentual from rendimentos GROUP BY(DataFim)
